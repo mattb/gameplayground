6c09ddbcc95477cdaf84cc6819947bb9d749cc51
Matt Biddulph <matt@hackdiary.com>
2009-06-07 14:18:36 +0100

    Walking freepixel sprite in its own scene.

--- Classes/MenuScene.m
+++ Classes/MenuScene.m
@@ -9,6 +9,7 @@
 #import "MenuScene.h"
 #import "MainScene.h"
 #import "PathScene.h"
+#import "FreepixelScene.h"
 
 @implementation MenuScene
 - (id) init
@@ -17,7 +18,8 @@
     if (self != nil) {
         MenuItem *menuItem1 = [MenuItemFont itemFromString:@"Main" target:self selector:@selector(onPlay:)];
         MenuItem *menuItem2 = [MenuItemFont itemFromString:@"Control Path" target:self selector:@selector(onPath:)];
-        Menu *menu = [Menu menuWithItems:menuItem1, menuItem2, nil];
+        MenuItem *menuItem3 = [MenuItemFont itemFromString:@"Freepixel Sprites" target:self selector:@selector(onFreepixel:)];
+        Menu *menu = [Menu menuWithItems:menuItem1, menuItem2, menuItem3, nil];
         [menu alignItemsVertically];
         [self addChild:menu];
     }
@@ -25,13 +27,15 @@
 }
 - (void)onPlay:(id)sender
 {
-    NSLog(@"on play");
     [[Director sharedDirector] replaceScene:[MainScene node]];
 }
 - (void)onPath:(id)sender
 {
-    NSLog(@"on path");
     [[Director sharedDirector] replaceScene:[PathScene node]];
+}    
+- (void)onFreepixel:(id)sender
+{
+    [[Director sharedDirector] replaceScene:[FreepixelScene node]];
 }    
 
 @end
